adfa51f6cb659160aa2dfa9c92e3e774c40f8ca6
brayo404 <brianoboka@gmail.com>
2017-01-18 22:53:02 +0300

    still going

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-
-function sayHello () {
+// DOM Manipulation
+document.addEventListener("DOMContentLoaded", 
+	function (event) {
+		function sayHello (event) {
+	console.log(event);
+	this.textContent = "Said it!";
 	var name = 
 	document.getElementById("name").value;
 	var message = "<h2>Hello " + name + "!</h2>";
@@ -7,4 +11,36 @@ function sayHello () {
 	document
 	.getElementById("content")
 	.innerHTML = message;
+
+	if (name === "student") {
+		var title = 
+		document.querySelector("#title")
+		.textContent;
+		title += " & Lovin' it!";
+		document.querySelector("#title")
+		.textContent = title;
+	}
 }
+
+document.querySelector("button")
+	.addEventListener("click", sayHello);
+
+	//document.querySelector("button")
+		//.onclick = sayHello;
+		document.querySelector("body")
+			.addEventListener("mousemove", 
+				function (event) {
+					if (event.shiftKey === true) {
+					console.log("X: " + event.clientX);
+					console.log("Y: " + event.clientY);
+				}}
+				);
+	
+	}
+	);
+
+
+
+
+
+
